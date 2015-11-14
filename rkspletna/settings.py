2567fdcf5a1f958cbb150b58a4b63c5f888d01d2
simon <simonrakovic@gmail.com>
2015-11-14 10:51:41 +0000

    popravek srvr

--- rkspletna/settings.py
+++ rkspletna/settings.py
@@ -81,6 +81,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = '/home/krakovic/webapps/rk_static/'
+
 STATIC_URL = '/static/'
 
 # Additional locations of static files
